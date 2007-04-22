37f7d29e90568702cc3950a7c6c1a2dd5e6edd69
Ariya Hidayat <ariya.hidayat@gmail.com>
2007-04-22 18:16:20 +0000

    new FSF address

--- src/number.h
+++ src/number.h
@@ -16,8 +16,8 @@
     along with this program; see the file COPYING.  If not, write to:
 
       The Free Software Foundation, Inc.
-      59 Temple Place, Suite 330
-      Boston, MA 02111-1307 USA.
+      51 Franklin Street, Fifth Floor,
+      Boston, MA 02110-1301, USA.
 
 
     You may contact the author by:
