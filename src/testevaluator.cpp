e33b06de8855e8ab27798d036302d78800ce813e
Ariya Hidayat <ariya.hidayat@gmail.com>
2007-04-27 10:10:48 +0000

    fix stupid round-trip

--- src/testevaluator.cpp
+++ src/testevaluator.cpp
@@ -33,16 +33,16 @@ static int eval_failed_tests = 0;
 #define CHECK_EVAL(x,y)  checkEval(__FILE__,__LINE__,#x,x,y)
 #define CHECK_EVAL_PRECISE(x,y)  checkEvalPrecise(__FILE__,__LINE__,#x,x,y)
 
-static void checkEval( const char *file, int line, const char* msg, 
+static void checkEval( const char *file, int line, const char* msg,
 const QString& expr, const char* expected )
 {
   eval_total_tests++;
 
   Evaluator e;
   e.setAngleMode(Evaluator::Radian);
-  e.setExpression(QString::fromLatin1(expr));
+  e.setExpression(expr);
   HNumber rn = e.eval();
-  
+
   if( !e.error().isEmpty() )
   {
     eval_failed_tests++;
@@ -63,18 +63,18 @@ const QString& expr, const char* expected )
       std::cerr << std::endl;
     }
     free( result );
-  }  
+  }
 }
 
-static void checkEvalPrecise( const char *file, int line, const char* msg, 
+static void checkEvalPrecise( const char *file, int line, const char* msg,
 const QString& expr, const char* expected )
 {
   eval_total_tests++;
 
   Evaluator e;
-  e.setExpression(QString::fromLatin1(expr));
+  e.setExpression(expr);
   HNumber rn = e.eval();
-  
+
   // we compare up to 50 decimals, not exact number because it's often difficult
   // to represent the result as rational numbers, e.g. PI
   char* result = HMath::formatFixed( rn, 50 );
@@ -104,25 +104,25 @@ void test_unary()
   CHECK_EVAL( "----1", "1" );
   CHECK_EVAL( "-----1", "-1" );
   CHECK_EVAL( "------1", "1" );
-  
+
   CHECK_EVAL( "-ABS(1)", "-1");
   CHECK_EVAL( "-ABS(-2)", "-2");
   CHECK_EVAL( "--ABS(-3)", "3");
   CHECK_EVAL( "---ABS(-4)", "-4");
-  
-  // see http://en.wikipedia.org/wiki/Empty_product    
-  CHECK_EVAL( "0^0", "1" ); 
-  
+
+  // see http://en.wikipedia.org/wiki/Empty_product
+  CHECK_EVAL( "0^0", "1" );
+
   CHECK_EVAL( "1^0", "1" );
   CHECK_EVAL( "1^1", "1" );
   CHECK_EVAL( "1^2", "1" );
   CHECK_EVAL( "1^3", "1" );
-  
+
   CHECK_EVAL( "-1^0", "1" );
   CHECK_EVAL( "-1^1", "-1" );
   CHECK_EVAL( "-1^2", "1" );
   CHECK_EVAL( "-1^3", "-1" );
-  
+
   CHECK_EVAL( "2^0", "1" );
   CHECK_EVAL( "2^1", "2" );
   CHECK_EVAL( "2^2", "4" );
@@ -134,7 +134,7 @@ void test_unary()
   CHECK_EVAL( "2^8", "256" );
   CHECK_EVAL( "2^9", "512" );
   CHECK_EVAL( "2^10", "1024" );
-  
+
   CHECK_EVAL( "-2^0", "1" );
   CHECK_EVAL( "-2^1", "-2" );
   CHECK_EVAL( "-2^2", "4" );
@@ -146,7 +146,7 @@ void test_unary()
   CHECK_EVAL( "-2^8", "256" );
   CHECK_EVAL( "-2^9", "-512" );
   CHECK_EVAL( "-2^10", "1024" );
-  
+
   CHECK_EVAL( "(5-7)^2", "4" );
 }
 
@@ -167,12 +167,12 @@ void test_function_basic()
   CHECK_EVAL( "ABS(---1)", "1" );
   CHECK_EVAL( "ABS((1))", "1" );
   CHECK_EVAL( "ABS((-1))", "1" );
-  
+
   CHECK_EVAL( "ABS(1/2)", "0.5" );
   CHECK_EVAL( "ABS(-1/2)", "0.5" );
   CHECK_EVAL( "ABS(-1/-2)", "0.5" );
   CHECK_EVAL( "ABS(1/-2)", "0.5" );
-  
+
   CHECK_EVAL( "INT(0)", "0" );
   CHECK_EVAL( "INT(1)", "1" );
   CHECK_EVAL( "INT(-1)", "-1" );
@@ -182,18 +182,18 @@ void test_function_basic()
   CHECK_EVAL( "INT(0.9999*1)", "0" );
   CHECK_EVAL( "INT(2.1)", "2" );
   CHECK_EVAL( "INT(-3.4)", "-3" );
-  
-  CHECK_EVAL_PRECISE( "exp((1)/2) + exp((1)/2)", "3.29744254140025629369730157562832714330755220142030" );  
+
+  CHECK_EVAL_PRECISE( "exp((1)/2) + exp((1)/2)", "3.29744254140025629369730157562832714330755220142030" );
 }
 
 void test_function_trig()
 {
   CHECK_EVAL_PRECISE( "pi", "3.14159265358979323846264338327950288419716939937511" );
-  
+
   CHECK_EVAL( "sin(0)", "0" );
   CHECK_EVAL( "cos(0)", "1" );
   CHECK_EVAL( "tan(0)", "0" );
-  
+
   CHECK_EVAL( "sin(pi)", "0" );
   CHECK_EVAL( "cos(pi)", "-1" );
   CHECK_EVAL( "tan(pi)", "0" );
@@ -217,16 +217,16 @@ void test_function_trig()
   CHECK_EVAL( "sin(-pi/2) - sin(pi/2)", "-2" );
   CHECK_EVAL( "cos(-pi/2) + cos(pi/2)", "0" );
   CHECK_EVAL( "cos(-pi/2) - cos(pi/2)", "0" );
-  
+
   CHECK_EVAL( "asin(sin(1))", "1");
   CHECK_EVAL( "acos(cos(1))", "1");
-  
+
   CHECK_EVAL( "degrees(0)", "0");
   CHECK_EVAL( "degrees(pi/2)", "90");
   CHECK_EVAL( "degrees(pi)", "180");
   CHECK_EVAL( "degrees(3*pi/2)", "270");
   CHECK_EVAL( "degrees(2*pi)", "360");
-  
+
   CHECK_EVAL( "radians(0)", "0" );
   CHECK_EVAL( "radians(90)/pi", "0.5" );
   CHECK_EVAL( "radians(180)/pi", "1" );
@@ -255,7 +255,7 @@ void test_function_stat()
   CHECK_EVAL( "MAX(0; 1; 2)", "2" );
   CHECK_EVAL( "MAX(-1; 0; 1; 2)", "2" );
   CHECK_EVAL( "MAX(-2; -1; 0; 1; 2)", "2" );
-  
+
   CHECK_EVAL( "SUM()", "0");
   CHECK_EVAL( "SUM(0)", "0");
   CHECK_EVAL( "SUM(1)", "1");
@@ -269,7 +269,7 @@ void test_function_stat()
   CHECK_EVAL( "SUM(-100;-1)", "-101");
   CHECK_EVAL( "SUM(1;2;3;4;5;6)", "21");
   CHECK_EVAL( "SUM(1;-2;3;-4;5;-6)", "-3");
-  
+
   CHECK_EVAL( "AVERAGE()", "NaN");
   CHECK_EVAL( "AVERAGE(0)", "0");
   CHECK_EVAL( "AVERAGE(0;0)", "0");
@@ -308,11 +308,11 @@ int main(int argc, char** argv)
 {
   eval_total_tests = 0;
   eval_failed_tests = 0;
-  
+
   test_constants();
   test_unary();
   test_binary();
-  
+
   test_function_basic();
   test_function_trig();
   test_function_stat();
