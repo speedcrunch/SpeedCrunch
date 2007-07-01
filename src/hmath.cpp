528c03e5139ff2e04b82412dd2b8248e2cb1470a
Helder Correia <helder.pereira.correia@gmail.com>
2007-07-01 13:57:45 +0000

    Update changelog. Next step will be 0.8alpha release.

--- src/hmath.cpp
+++ src/hmath.cpp
@@ -1,7 +1,6 @@
 /* HMath: C++ high precision math routines
    Copyright (C) 2004 Ariya Hidayat <ariya.hidayat@gmail.com>
                  2007 Helder Correia <helder.pereira.correia@gmail.com>
-   Last update: June 11, 2007
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
