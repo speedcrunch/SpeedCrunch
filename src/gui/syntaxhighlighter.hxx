e24a6559c1684bd8b8bf1f9c89da8cc5d262e9cf
Helder Correia <helder.pereira.correia@gmail.com>
2009-04-04 20:25:41 +0200

    Just being picky about copyright dates.

--- src/gui/syntaxhighlighter.hxx
+++ src/gui/syntaxhighlighter.hxx
@@ -1,6 +1,6 @@
 // This file is part of the SpeedCrunch project
 // Copyright (C) 2007 Ariya Hidayat <ariya@kde.org>
-// Copyright (C) 2009 Helder Correia <helder.pereira.correia@gmail.com>
+// Copyright (C) 2007-2009 Helder Correia <helder.pereira.correia@gmail.com>
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
