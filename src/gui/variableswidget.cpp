1b9b19dbf7167ef979f64d6cca21ada8a744c23b
eckorog2005 <eckorog2005@gmail.com>
2012-03-21 21:05:04 -0300

    Update src/gui/variableswidget.cpp

--- src/gui/variableswidget.cpp
+++ src/gui/variableswidget.cpp
@@ -1,6 +1,7 @@
 // This file is part of the SpeedCrunch project
 // Copyright (C) 2009 Andreas Scherer <andreas_coder@freenet.de>
 // Copyright (C) 2009, 2011 Helder Correia <helder.pereira.correia@gmail.com>
+// Copyright (C) 2012 Roger Lamb <rlamb1id@gmail.com>
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
