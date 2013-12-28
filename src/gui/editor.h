880705bd1d798b80d44b0d898f97b49fbbf4ad10
Alessandro Portale <alessandro@casaportale.de>
2013-12-28 22:04:32 +0100

    Polish the merged copy behavior of editor and display
    Now, text selection can exist on either editor or display,
    never both. And the curently selected text (on either one
    of the widgets) gets copied to clipboard, when the copy
    key sequence or Ctrl-C is triggered.
    In short both widgets act like one combined widget when it
    comes to copying text to clipboard.
    
    This path removed the automatic copy-on-selection feature
    that the display had.

--- src/gui/editor.h
+++ src/gui/editor.h
@@ -72,6 +72,7 @@ signals:
     void autoCalcDisabled();
     void controlPageDownPressed();
     void controlPageUpPressed();
+    void copySequencePressed();
     void pageDownPressed();
     void pageUpPressed();
     void returnPressed();
