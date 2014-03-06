speedcrunch/SpeedCrunch

commit aa268a4e2aa8a8407db55a84393c140929f58044

Adopt ISO syntax for logarithmic functions
https://code.google.com/p/speedcrunch/issues/detail?id=381

Conversions in this change:
    lg  -> lb
    log -> lg
    ln  -> ln (was already correct)

src/core/functions.cpp
@@ -100,10 +100,10 @@ static HNumber function_hyperpmf(Function*, const Function::ArgumentList&);
 static HNumber function_hypervar(Function*, const Function::ArgumentList&);
 static HNumber function_idiv(Function*, const Function::ArgumentList&);
 static HNumber function_integer(Function*, const Function::ArgumentList&);
-static HNumber function_lg(Function*, const Function::ArgumentList&);
+static HNumber function_lb(Function*, const Function::ArgumentList&);
 static HNumber function_ln(Function*, const Function::ArgumentList&);
 static HNumber function_lnGamma(Function*, const Function::ArgumentList&);
-static HNumber function_log(Function*, const Function::ArgumentList&);
+static HNumber function_lg(Function*, const Function::ArgumentList&);
 static HNumber function_mask(Function*, const Function::ArgumentList&);
 static HNumber function_max(Function*, const Function::ArgumentList&);
 static HNumber function_median(Function*, const Function::ArgumentList&);
@@ -273,13 +273,13 @@ HNumber function_ln(Function* f, const Function::ArgumentList& args)
     return HMath::ln(args.at(0));
 }
 
-HNumber function_log(Function* f, const Function::ArgumentList& args)
+HNumber function_lg(Function* f, const Function::ArgumentList& args)
 {
     ENSURE_ARGUMENT_COUNT(1);
-    return HMath::log(args.at(0));
+    return HMath::lg(args.at(0));
 }
 
-HNumber function_lg(Function* f, const Function::ArgumentList& args)
+HNumber function_lb(Function* f, const Function::ArgumentList& args)
 {
     ENSURE_ARGUMENT_COUNT(1);
     return HMath::lg(args.at(0));
@@ -757,9 +757,9 @@ void FunctionRepo::createFunctions()
     insert(new Function("csc", function_csc, this));
     insert(new Function("degrees", function_degrees, this));
     insert(new Function("exp", function_exp, this));
+    insert(new Function("lb", function_lb, this));
     insert(new Function("lg", function_lg, this));
     insert(new Function("ln", function_ln, this));
-    insert(new Function("log", function_log, this));
     insert(new Function("radians", function_radians, this));
     insert(new Function("sec", function_sec, this));
     insert(new Function("sin", function_sin, this));
@@ -850,10 +850,10 @@ void FunctionRepo::setNonTranslatableFunctionUsages()
     find("geomean")->setUsage(QLatin1String("x<sub>1</sub>; x<sub>2</sub>; ..."));
     find("hex")->setUsage(QString::fromLatin1("n"));
     find("int")->setUsage(QString::fromLatin1("x"));
+    find("lb")->setUsage(QString::fromLatin1("x"));
     find("lg")->setUsage(QString::fromLatin1("x"));
     find("ln")->setUsage(QString::fromLatin1("x"));
     find("lngamma")->setUsage(QString::fromLatin1("x"));
-    find("log")->setUsage(QString::fromLatin1("x"));
     find("max")->setUsage(QLatin1String("x<sub>1</sub>; x<sub>2</sub>; ..."));
     find("median")->setUsage(QLatin1String("x<sub>1</sub>; x<sub>2</sub>; ..."));
     find("min")->setUsage(QLatin1String("x<sub>1</sub>; x<sub>2</sub>; ..."));
@@ -941,10 +941,10 @@ void FunctionRepo::setFunctionNames()
     find("hypervar")->setName(Function::tr("Hypergeometric Distribution Variance"));
     find("idiv")->setName(Function::tr("Integer Quotient"));
     find("int")->setName(Function::tr("Integer Part"));
-    find("lg")->setName(Function::tr("Base-2 Logarithm"));
+    find("lb")->setName(Function::tr("Binary Logarithm"));
+    find("lg")->setName(Function::tr("Common Logarithm"));
     find("ln")->setName(Function::tr("Natural Logarithm"));
     find("lngamma")->setName("ln(abs(Gamma))");
-    find("log")->setName(Function::tr("Base-10 Logarithm"));
     find("mask")->setName(Function::tr("Mask to a bit size"));
     find("max")->setName(Function::tr("Maximum"));
     find("median")->setName(Function::tr("Median Value (50th Percentile)"));

src/tests/testevaluator.cpp
@@ -253,7 +253,7 @@ void test_function_basic()
     CHECK_EVAL("(1+1)!^2", "4");
     CHECK_EVAL_KNOWN_ISSUE("frac 3!", "0", 451);
 
-    CHECK_EVAL_KNOWN_ISSUE("log 10^2", "2", 451);
+    CHECK_EVAL_KNOWN_ISSUE("lg 10^2", "2", 451);
 
     CHECK_EVAL("(-27)^(1/3)", "-3");
     CHECK_EVAL("(-27)^(-1/3)", "-0.33333333333333333333");
@@ -495,7 +495,7 @@ void test_auto_fix_ans()
     CHECK_AUTOFIX("tan", "tan(ans)");
     CHECK_AUTOFIX("abs", "abs(ans)");
     CHECK_AUTOFIX("exp", "exp(ans)");
-    CHECK_AUTOFIX("log", "log(ans)");
+    CHECK_AUTOFIX("lg", "lg(ans)");
 }
 
 void test_auto_fix_trailing_equal()

src/tests/testhmath.cpp
@@ -716,37 +716,37 @@ void test_functions()
   CHECK_PRECISE( HMath::ln( "100" ),
                  "4.60517018598809136803598290936872841520220297725755"  );
 
-  // log
-  CHECK( HMath::log( "NaN"   ), "NaN" );
-  CHECK( HMath::log( "-1"    ), "NaN" );
-  CHECK( HMath::log( "0"     ), "NaN" );
-  CHECK( HMath::log( "1e-5"  ), "-5"  );
-  CHECK( HMath::log( "1e-4"  ), "-4"  );
-  CHECK( HMath::log( "1e-3"  ), "-3"  );
-  CHECK( HMath::log( "10"    ), "1"   );
-  CHECK( HMath::log( "100"   ), "2"   );
-  CHECK( HMath::log( "1000"  ), "3"   );
-  CHECK( HMath::log( "10000" ), "4"   );
-  CHECK( HMath::log( "1e5"   ), "5"   );
-  CHECK( HMath::log( "1e6"   ), "6"   );
-  CHECK( HMath::log( "1e7"   ), "7"   );
-  CHECK( HMath::log( "1e8"   ), "8"   );
-  CHECK( HMath::log( "1e9"   ), "9"   );
-  CHECK( HMath::log( "1e10"  ), "10"  );
-  CHECK( HMath::log( "0.00000000001" ), "-11" );
-
   // lg
-  CHECK( HMath::lg( "NaN" ), "NaN" );
-  CHECK( HMath::lg( "-1"  ), "NaN" );
-  CHECK( HMath::lg( "0"   ), "NaN" );
-  CHECK( HMath::lg( "1"   ), "0"   );
-  CHECK( HMath::lg( "2"   ), "1"   );
-  CHECK( HMath::lg( "4"   ), "2"   );
-  CHECK( HMath::lg( "8"   ), "3"   );
-  CHECK( HMath::lg( "16"  ), "4"   );
-  CHECK( HMath::lg( "32"  ), "5"   );
-  CHECK( HMath::lg( "32"  ), "5"   );
-  CHECK( HMath::lg( "0.00000000001" ), "-36.54120904376098582657" );
+  CHECK( HMath::lg( "NaN"   ), "NaN" );
+  CHECK( HMath::lg( "-1"    ), "NaN" );
+  CHECK( HMath::lg( "0"     ), "NaN" );
+  CHECK( HMath::lg( "1e-5"  ), "-5"  );
+  CHECK( HMath::lg( "1e-4"  ), "-4"  );
+  CHECK( HMath::lg( "1e-3"  ), "-3"  );
+  CHECK( HMath::lg( "10"    ), "1"   );
+  CHECK( HMath::lg( "100"   ), "2"   );
+  CHECK( HMath::lg( "1000"  ), "3"   );
+  CHECK( HMath::lg( "10000" ), "4"   );
+  CHECK( HMath::lg( "1e5"   ), "5"   );
+  CHECK( HMath::lg( "1e6"   ), "6"   );
+  CHECK( HMath::lg( "1e7"   ), "7"   );
+  CHECK( HMath::lg( "1e8"   ), "8"   );
+  CHECK( HMath::lg( "1e9"   ), "9"   );
+  CHECK( HMath::lg( "1e10"  ), "10"  );
+  CHECK( HMath::lg( "0.00000000001" ), "-11" );
+
+  // lb
+  CHECK( HMath::lb( "NaN" ), "NaN" );
+  CHECK( HMath::lb( "-1"  ), "NaN" );
+  CHECK( HMath::lb( "0"   ), "NaN" );
+  CHECK( HMath::lb( "1"   ), "0"   );
+  CHECK( HMath::lb( "2"   ), "1"   );
+  CHECK( HMath::lb( "4"   ), "2"   );
+  CHECK( HMath::lb( "8"   ), "3"   );
+  CHECK( HMath::lb( "16"  ), "4"   );
+  CHECK( HMath::lb( "32"  ), "5"   );
+  CHECK( HMath::lb( "32"  ), "5"   );
+  CHECK( HMath::lb( "0.00000000001" ), "-36.54120904376098582657" );
 
   // sin
   CHECK( HMath::sin( "NaN"   ), "NaN" );

src/math/floathmath.c
@@ -152,7 +152,7 @@ float_arcosh(
 }
 
 char
-float_log(
+float_lg(
   floatnum x,
   int digits)
 {
@@ -175,7 +175,7 @@ float_log(
 }
 
 char
-float_lg(
+float_lb(
   floatnum x,
   int digits)
 {

src/math/floathmath.h
@@ -30,7 +30,7 @@
 *************************************************************************/
 
 #ifndef FLOATHMATH_H
-# define FLOATHMATH_H
+#define FLOATHMATH_H
 
 #include "floatnum.h"
 
@@ -47,27 +47,27 @@ extern "C" {
            OutOfDomain */
 char float_lnxplus1(floatnum x, int digits);
 
-/* evaluates ln x for x > 0. Near x == 0, this function yields better
-   results than float_lnxplus1.
+/* evaluates ln x (the logarithm to base e) for x > 0.
+   Near x == 0, this function yields better results than float_lnxplus1.
    In case of an error, x is set to NaN and 0 is returned.
    Errors: NaNOperand
            InvalidPrecision (digits > MATHPRECISION)
            OutOfDomain */
 char float_ln(floatnum x, int digits);
 
-/* evaluates log x (the logarithm to base 10) for x > 0.
-   In case of an error, x is set to NaN and 0 is returned.
-   Errors: NaNOperand
-           InvalidPrecision (digits > MATHPRECISION)
-           OutOfDomain */
-char float_log(floatnum x, int digits);
-
-/* evaluates lg x (the logarithm to base 2) for x > 0.
+/* evaluates lg x (the logarithm to base 10) for x > 0.
    In case of an error, x is set to NaN and 0 is returned.
    Errors: NaNOperand
            InvalidPrecision (digits > MATHPRECISION)
            OutOfDomain */
 char float_lg(floatnum x, int digits);
+
+/* evaluates lb x (the logarithm to base 2) for x > 0.
+   In case of an error, x is set to NaN and 0 is returned.
+   Errors: NaNOperand
+           InvalidPrecision (digits > MATHPRECISION)
+           OutOfDomain */
+char float_lb(floatnum x, int digits);
 
 /* evaluates arsinh x, the inverse function of sinh.
    In case of an error, x is set to NaN and 0 is returned.

src/math/hmath.cpp
@@ -1138,24 +1138,24 @@ HNumber HMath::ln( const HNumber & x )
 }
 
 /**
- * Returns the base-10 logarithm of x.
- * If x is non positive, returns NaN.
- */
-HNumber HMath::log( const HNumber & x )
-{
-  HNumber result;
-  call1Arg(result.d, x.d, float_log);
-  return result;
-}
-
-/**
- * Returns the base-2 logarithm of x.
+ * Returns the common logarithm of x.
  * If x is non positive, returns NaN.
  */
 HNumber HMath::lg( const HNumber & x )
 {
   HNumber result;
   call1Arg(result.d, x.d, float_lg);
+  return result;
+}
+
+/**
+ * Returns the binary logarithm of x.
+ * If x is non positive, returns NaN.
+ */
+HNumber HMath::lb( const HNumber & x )
+{
+  HNumber result;
+  call1Arg(result.d, x.d, float_lb);
   return result;
 }
 

src/math/hmath.h
@@ -125,8 +125,8 @@ class HMath
     // EXPONENTIAL FUNCTION AND RELATED
     static HNumber exp( const HNumber & x );
     static HNumber ln( const HNumber & x );
-    static HNumber log( const HNumber & x );
     static HNumber lg( const HNumber & x );
+    static HNumber lb( const HNumber & x );
     static HNumber sinh( const HNumber & x );
     static HNumber cosh( const HNumber & x );
     static HNumber tanh( const HNumber & x );

commit 4617123ab527e0a8a9d16e7f657e1a631870e9aa

Fix typo that causes lb(x) to give lg(x) results
Bug was introduced in change aa268a4e because testevaluator didn't cover
logarithmic functions.

src/core/functions.cpp
@@ -283,7 +283,7 @@ HNumber function_lg(Function* f, const Function::ArgumentList& args)
 HNumber function_lb(Function* f, const Function::ArgumentList& args)
 {
     ENSURE_ARGUMENT_COUNT(1);
-    return HMath::lg(args.at(0));
+    return HMath::lb(args.at(0));
 }
 
 HNumber function_log(Function* f, const Function::ArgumentList& args)

src/tests/testevaluator.cpp
@@ -242,6 +242,14 @@ void test_function_basic()
     CHECK_EVAL("INT(2.1)", "2");
     CHECK_EVAL("INT(-3.4)", "-3");
 
+    CHECK_EVAL("log(0.123; 0.1234)", "0.99845065797473594741");
+    CHECK_EVAL("lg(0.00000000001)", "-11");
+    CHECK_EVAL("lg(1e-3)", "-3");
+    CHECK_EVAL("lb(0.00000000001)", "-36.54120904376098582657");
+    CHECK_EVAL("lb(32)", "5");
+    CHECK_EVAL("ln(100)", "4.60517018598809136804");
+    CHECK_EVAL("ln(4.0)", "1.38629436111989061883");
+
     CHECK_EVAL("0!", "1");
     CHECK_EVAL("1!", "1");
     CHECK_EVAL("2!", "2");

src/tests/testhmath.cpp
@@ -745,7 +745,6 @@ void test_functions()
   CHECK( HMath::lb( "8"   ), "3"   );
   CHECK( HMath::lb( "16"  ), "4"   );
   CHECK( HMath::lb( "32"  ), "5"   );
-  CHECK( HMath::lb( "32"  ), "5"   );
   CHECK( HMath::lb( "0.00000000001" ), "-36.54120904376098582657" );
 
   // log